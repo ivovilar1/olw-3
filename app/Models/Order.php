9d53694c6b09d86a1ff2872a141433084a59885e
Ivo <ivo_vilar@hotmail.com>
2023-09-13 09:19:41 -0300

    corrigido alguns problemas com os relacionamentos

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -29,7 +29,9 @@ class Order extends Model
     }
     public function skus(): BelongsToMany
     {
-        return $this->belongsToMany(Sku::class)->using(OrderSku::class);
+        return $this->belongsToMany(Sku::class)
+                    ->using(OrderSku::class)
+                    ->withPivot('quantity', 'unitary_price', 'product');
     }
 
     public function payments(): HasMany
